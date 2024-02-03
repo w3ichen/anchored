49de2eb2185d133bd3a1e86930ffbb5ee2475fc8
Weichen Qiu <57570491+w3ichen@users.noreply.github.com>
2024-02-03 13:23:59 -0500

    user profile is kinda done

---
+++ front-end/pages/deeper.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import Page from "../components/Page";
+
+export default function DivingDeeper() {
+  return (
+    <Page>
+      <Typography variant="h1">Diving Deeper</Typography>
+    </Page>
+  );
+}
---
+++ front-end/pages/find.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import Page from "../components/Page";
+
+export default function FindFriends() {
+  return (
+    <Page>
+      <Typography variant="h1">Find Friends</Typography>
+    </Page>
+  );
+}
