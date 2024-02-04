13c233060184eb4c57595087f9b653c8cce89bd3
Weichen Qiu <57570491+w3ichen@users.noreply.github.com>
2024-02-04 13:04:55 -0500

    set basePath

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -11,4 +11,6 @@ module.exports = withPWA({
     skipWaiting: true,
   },
   target: "serverless", // will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
+  basePath: "/anchored",
+  assetPrefix: "/anchored/",
 });
