30a7718753d492dd10fed4ff098efb4d26f6edee
Weichen Qiu <57570491+w3ichen@users.noreply.github.com>
2024-02-04 12:38:04 -0500

    target serverless

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -10,4 +10,5 @@ module.exports = withPWA({
     register: true,
     skipWaiting: true,
   },
+  target: "serverless", // will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
 });
