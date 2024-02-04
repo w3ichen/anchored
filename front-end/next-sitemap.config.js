2a8b6b7b6d4295175856ed29f424f15dc40585ea
Weichen Qiu <57570491+w3ichen@users.noreply.github.com>
2024-02-04 13:35:24 -0500

    made more changes

---
+++ front-end/next-sitemap.config.js
@@ -0,0 +1,6 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: "https://w3ichen.github.io/anchored/",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -6,6 +6,10 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Anchored</title>
+        <meta name="description" content="Friendships on a Deeper Level" />
+        <meta name="og:title" content="Anchored" />
+        <meta name="og:description" content="Friendships on a Deeper Level" />
+        <meta name="og:image" content="full_image_1.png" />
       </Head>
       <Component {...pageProps} />
     </>
